5c15bb7b5c55e372385b2c899fadac0dcdf6ca21
Maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-07-21 21:17:27 +0300

    update adjusting modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,14 +2,15 @@ import { createPortal } from 'react-dom';
 
 export const Modal = ({ children, setShowModal }) => {
   const modalStyle = 'p-4 rounded-md bg-orange-900 mx-4';
-  const modalOverlayStyle =
-    'fixed z-20 w-full h-full bg-black bg-opacity-70 top-0 left-0 inset-0';
+  const modalOverlayStyle = 'fixed z-20 bg-black bg-opacity-70 inset-0';
   return createPortal(
     <div
       onClick={() => setShowModal(false)}
       className={`${modalOverlayStyle} flex items-center justify-center`}
     >
-      <div className={modalStyle}>{children}</div>
+      <div onClick={(e) => e.stopPropagation()} className={modalStyle}>
+        {children}
+      </div>
     </div>,
     document.getElementById('modal'),
   );
